3482cee84afb3fedcd45e1383f0add05f89d2488
Andrej Benz <hello@benz.dev>
2021-03-14 20:39:09 +0100

    changed gitsigns symbols

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -102,7 +102,42 @@ return require('packer').startup(function(use)
   use {
     'lewis6991/gitsigns.nvim',
     requires = {'nvim-lua/plenary.nvim'},
-    config = function() require('gitsigns').setup() end
+    config = function()
+      require('gitsigns').setup {
+        signs = {
+          add = {
+            hl = 'GitSignsAdd',
+            text = '▉',
+            numhl = 'GitSignsAddNr',
+            linehl = 'GitSignsAddLn'
+          },
+          change = {
+            hl = 'GitSignsChange',
+            text = '▉',
+            numhl = 'GitSignsChangeNr',
+            linehl = 'GitSignsChangeLn'
+          },
+          delete = {
+            hl = 'GitSignsDelete',
+            text = '▉',
+            numhl = 'GitSignsDeleteNr',
+            linehl = 'GitSignsDeleteLn'
+          },
+          topdelete = {
+            hl = 'GitSignsDelete',
+            text = '‾',
+            numhl = 'GitSignsDeleteNr',
+            linehl = 'GitSignsDeleteLn'
+          },
+          changedelete = {
+            hl = 'GitSignsChange',
+            text = '~',
+            numhl = 'GitSignsChangeNr',
+            linehl = 'GitSignsChangeLn'
+          }
+        }
+      }
+    end
   }
 
   use {
--- lua/theme.lua
+++ lua/theme.lua
@@ -5,9 +5,9 @@ local _, colors, Group, _, styles = require('colorbuddy').setup()
 Group.new('SignColumn', colors.none, colors.none, styles.NONE)
 Group.new('NormalFloat', colors.none, colors.pmenu, styles.NONE)
 
-Group.new('GitSignsAdd', colors.hue_4, colors.hue_4, styles.NONE)
-Group.new('GitSignsChange', colors.orange, colors.orange, styles.NONE)
-Group.new('GitSignsDelete', colors.hue_5_2, colors.hue_5_2, styles.NONE)
+Group.new('GitSignsAdd', colors.hue_4, colors.none, styles.NONE)
+Group.new('GitSignsChange', colors.orange, colors.none, styles.NONE)
+Group.new('GitSignsDelete', colors.hue_5_2, colors.none, styles.NONE)
 
 Group.new('BufferTabpageFill', colors.none, colors.syntax_bg, styles.NONE)
 
